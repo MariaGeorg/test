da6ec536cefdb149f0238a1e9afd6d39ffa89a03
MariaGeorg <maria.georgopoulou@live.se>
2016-05-04 23:10:43 +0200

    first
    first

---
+++ text.php
@@ -0,0 +1,11 @@
+<script>
+
+
+    $getJSON{'http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?nyckelord=stockholm',{gueryString:'value'}, function(response){
+        consolor.log(response);
+    }
+
+    }
+
+
+</script>
